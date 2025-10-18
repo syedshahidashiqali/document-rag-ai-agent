4e28a23a74e0cad43129d69d333e6a1570dc495e
Syed Shahid Ashiq Ali <syedshahid1215@gmail.com>
2025-10-19 00:27:47 +0500

    Inngest Dev Server Setup

---
+++ main.py
@@ -0,0 +1,30 @@
+import logging
+from fastapi import FastAPI
+import inngest
+import inngest.fast_api
+from inngest.experimental import ai
+from dotenv import load_dotenv
+import uuid
+import os
+import datetime
+
+load_dotenv()
+
+inngest_client = inngest.Inngest(
+  app_id="rag_app",
+  logger=logging.getLogger("uvicon"),
+  is_production=False,
+  serializer=inngest.PydanticSerializer,
+)
+
+
+@inngest_client.create_function(
+  fn_id="RAG Ingest PDF",
+  trigger=inngest.TriggerEvent(event="rag/ingest_pdf")
+)
+async def rag_ingest_pdf(ctx: inngest.Context):
+  return {"hello": "world"}
+
+app  = FastAPI()
+
+inngest.fast_api.serve(app,inngest_client,[rag_ingest_pdf])
